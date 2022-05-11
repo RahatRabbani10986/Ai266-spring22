05f48be9ca901f96efeb7d54171de3d025b7c5ab
RahatRabbani10986 <99352810+RahatRabbani10986@users.noreply.github.com>
2022-05-11 22:16:19 +0500

    Update code.py

--- Assignment 2/code.py
+++ Assignment 2/code.py
@@ -1,1 +1,20 @@
+#Date:May 11,2022
+#---Importing Libraries---#
+import pandas as pd
+import numpy as np
 
+#-----Reading test Data---#
+testDf = pd.read_csv('sample.csv')
+
+#-----Getting Id and removing everything else----#
+idDf = testDf[['id']];
+
+#----Creating target column as per kaggle----#
+idDf.insert(1,"target",0)
+
+#----Generating Random Values-----#
+idDf['target']=np.random.rand(700000,1);
+
+#----Writing Back in Csv File----#
+idDf.to_csv('sample_file.csv');
+print(idDf);
