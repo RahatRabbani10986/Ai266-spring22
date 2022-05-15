90841cf9b1b30de0a5abac8d6cf6b0257f4d3da7
salahshakeel <99357263+salahshakeel@users.noreply.github.com>
2022-05-15 11:40:06 +0500

    Update code.py

--- Assignment 2/code.py
+++ Assignment 2/code.py
@@ -4,18 +4,19 @@ import pandas as pd
 import numpy as np
 
 
-#-----Reading test Data---#
+#-----This line is reading test Data---#
 testDf = pd.read_csv('sample.csv')
 
-#-----Getting Id and removing everything else----#
+#-----In this peice of code we are getting ID and removing everything else----#
 idDf = testDf[['id']];
 
-#----Creating target column as per kaggle----#
+#----This line is creating target column as per kaggle----#
 idDf.insert(1,"target",0)
 
-#----Generating Random Values-----#
+#----This line is generating random values-----#
 idDf['target']=np.random.rand(700000,1);
 
 #----Writing Back in Csv File----#
 idDf.to_csv('sample_file.csv');
+
 print(idDf);
